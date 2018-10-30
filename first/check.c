9dde7734cb11f4ec0648e31aa69c47c0d9e718a3
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-30 11:09:30 +0100

    Create check.c

---
+++ first/check.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    
+    system("./main");
+    
+    return 0;
+}
