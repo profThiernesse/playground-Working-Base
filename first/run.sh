8d979c5700427a327d8916a6e2988e62d0e6fbee
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-30 13:50:57 +0100

    Update run.sh

--- first/run.sh
+++ first/run.sh
@@ -20,7 +20,7 @@ echo "TECHIO> redirect-streams \"MORE\""
 '
 
 for file in test/TEST_*.in; do
-  ./check "./"$1 "test/"$file".in" "output.txt"
+  ./check "./"$1 $file "output.txt"
   echo "TECHIO> redirect-streams \"File output\"" 
   tail output.txt
   result=0
