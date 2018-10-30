aef48d3424da2bc7baf37fd59057b27457ceed9b
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-30 11:28:35 +0100

    Update run.sh

--- first/run.sh
+++ first/run.sh
@@ -3,12 +3,14 @@ gcc check.c -o check
 make  
 #./premier
 ./check
-echo "TECHIO> redirect-streams \"Test $1\"" 
+echo "TECHIO> redirect-streams \"File output $1\"" 
 tail output.txt
 if (diff -d -b -B output.txt check.txt > output.nfo); then
-  echo "TECHIO> message TEST x passé"
+  echo "TECHIO> message --channel \"Test Result\" TEST x passé"
   echo "TECHIO> success true"
 else
-  echo "TECHIO> message TEST x échoué"
+  echo "TECHIO> message --channel \"Test Result\" TEST x échoué"
   echo "TECHIO> success false"
+  echo "TECHIO> redirect-streams \"Difference\""
+  tail output.nfo
 fi
