89f200612fc34203650926047e2ad7acbab1f231
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-30 14:22:05 +0100

    Update run.sh

--- first/run.sh
+++ first/run.sh
@@ -4,21 +4,21 @@ make
 
 for file in test/TEST_*.in; do
   ./check "./"$1 $file "output.txt"
-  echo "TECHIO> redirect-streams \"File output\"" 
-  #tail output.txt
+  echo "TECHIO> redirect-streams \"Execution output\"" 
+  tail output.txt
   result=true
   if (diff -d -b -B output.txt $file".out" > output.nfo); then
-    echo "TECHIO> message --channel \"Test Result\" $file passé"
+    echo "TECHIO> message --channel \"Testing Results\" $file passé"
     #echo "TECHIO> success true"
   else
-    echo "TECHIO> message --channel \"Test Result\" $file échoué"
+    echo "TECHIO> message --channel \"Testing Results\" $file échoué"
     #echo "TECHIO> success false"
     #echo "TECHIO> redirect-streams \"Difference\""
     #tail output.nfo
     result=false
   fi  
 done
-zero=0
+
 if [ "$result" = true ]; then
   echo "TECHIO> success true"
 else
