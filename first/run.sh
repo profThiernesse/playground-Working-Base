fd9e5eea98986a1b582dd17808abf8429b843445
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-30 10:51:47 +0100

    Update run.sh

--- first/run.sh
+++ first/run.sh
@@ -3,7 +3,7 @@ make
 ./premier
 echo "TECHIO> redirect-streams \"Test $1\"" 
 tail output.txt
-if [diff output.txt check.txt]; then
+if [$(diff output.txt check.txt)]; then
   echo "TECHIO> success true"
 else
   echo "TECHIO> success false"
