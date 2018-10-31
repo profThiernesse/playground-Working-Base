843871d56b1cee135215da6bc35af84057a53956
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-31 08:12:20 +0100

    Update premier.c

--- first/premier.c
+++ first/premier.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main() {
-  printf("Coucou\n");
+int bissex(int);
+
+int main() {
+  int a;
+  
+  scanf("%d",&a);
+  
+  printf("%d",bissex(a));
+  
+  return 0;
+}
+
+int bissex(int a) {
+    return (a%400==0 || (a%100!=0 && a%4==0))
 }
